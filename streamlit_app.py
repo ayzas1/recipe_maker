593870aab7e1480fd764508e43a3dba3533afb61
ayzas1 <132793383+ayzas1@users.noreply.github.com>
2024-12-16 23:20:47 +0000

    UPDATE (MAIN)

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,12 @@
 import streamlit as st
+import supabase
+from dotenv import load_dotenv
+load_dotenv()
+st.title("🎈 Recipe Maker")
+email = st.text_input("Email")
+password = st.text_input("Password", None, 18, None, 'password', None, None, None, None, None)
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+signup = st.button("Sign Up")
+
+if signup == True:
+    st.write("sigma")
